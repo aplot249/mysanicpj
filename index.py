db6ffd38d13e89f060124ff07229e9eea843be23
qq1788lover <a17868810687@gmail.com>
2021-06-30 09:38:09 +0000

    过期发邮件

--- index.py
+++ index.py
@@ -101,4 +101,4 @@ async def expire(request):
 
 if __name__ == "__main__":
     app.error_handler.add(NotFound,lambda r, e: empty(status=404))
-    app.run(host='0.0.0.0', port=8008,protocol=WebSocketProtocol,auto_reload=True)
+    app.run(host='127.0.0.1', port=8008,protocol=WebSocketProtocol,auto_reload=True)
